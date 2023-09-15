6d37cff4d09e2cbbc50a8951678654c2d325fb68
Clark Sell <clark.sell@csell.net>
2023-09-15 16:15:04 -0500

    fix: markdown file extensions

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const dev = process.env.NODE_ENV === 'development' ? true : false;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	kit: {
 		serviceWorker: {
 			register: false
