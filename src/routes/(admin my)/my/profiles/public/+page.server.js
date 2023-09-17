ccc8868c00ee8bbcd8e9de14ec7dc36022ace2a1
Clark Sell <clark.sell@csell.net>
2023-09-16 19:45:07 -0500

    refactor: public profile updates

--- src/routes/(admin my)/my/profiles/public/+page.server.js
+++ src/routes/(admin my)/my/profiles/public/+page.server.js
@@ -1,48 +1,28 @@
-import lodash from 'lodash';
 import { fail } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
+import { redirect, setFlash } from 'sveltekit-flash-message/server';
+
+import memberApi from '$dataSources/api.that.tech/members/mutations';
 import meApi from '$dataSources/api.that.tech/me';
 
 import publicProfileSchema from '$lib/formSchemas/publicProfile';
 
-const { isNil, isEmpty } = lodash;
-
-export const load = async ({ parent, fetch }) => {
-	console.log('in load');
+export const load = async ({ fetch }) => {
 	const { queryMe } = meApi(fetch);
-	const me = await queryMe(fetch);
+	const me = await queryMe();
 
-	let isNewProfile, currentProfile;
-	let { user } = await parent();
-
-	if (user || me) {
-		if (!isNil(me) && !isEmpty(me)) {
-			currentProfile = me;
-			isNewProfile = false;
-		} else {
-			currentProfile = {
-				firstName: user.baseUser?.given_name ? user.baseUser.given_name : '',
-				lastName: user.baseUser?.family_name ? user.baseUser.family_name : '',
-				profileSlug: user.baseUser?.nickname ? user.baseUser.nickname : '',
-				email: user.baseUser?.email ? user.baseUser.email : ''
-			};
-			isNewProfile = true;
-		}
-	}
-
-	const form = await superValidate(currentProfile, publicProfileSchema);
+	const form = await superValidate(me, publicProfileSchema);
 
 	return {
-		form,
-		isNewProfile,
-		currentProfile
+		form
 	};
 };
 
 export const actions = {
-	default: async ({ request }) => {
-		const form = await superValidate(request, publicProfileSchema);
-		console.log('POST', JSON.stringify(form.data));
+	default: async (event) => {
+		const { updateProfile } = memberApi(event.fetch);
+
+		const form = await superValidate(event, publicProfileSchema);
 
 		if (!form.valid) {
 			return fail(400, {
@@ -50,6 +30,23 @@ export const actions = {
 			});
 		}
 
-		return { form };
+		try {
+			await updateProfile(form.data);
+
+			const successMessage = {
+				type: 'success',
+				message: `Your public profile has been updated.`
+			};
+
+			setFlash(successMessage, event);
+			return { form };
+		} catch (error) {
+			const errorMessage = {
+				type: 'error',
+				message: `Whoops!!! ${error.message}`
+			};
+
+			throw redirect(errorMessage, event);
+		}
 	}
 };
