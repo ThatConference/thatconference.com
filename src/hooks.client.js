92e40cc702607b034a143e2b18920b203980e1fc
Brett Slaski <brett@thatconference.com>
2023-09-29 14:58:12 -0500

    refactor: environment variable use of node_env. Not exposed by sveltekit

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -1,11 +1,13 @@
 import * as Sentry from '@sentry/sveltekit';
+import { env } from '$env/dynamic/public';
 
 Sentry.init({
 	dsn: 'https://857800ed593d481bb0da2843516d7845@o235190.ingest.sentry.io/4504617287417856',
 	tracesSampleRate: 1,
 	replaysSessionSampleRate: 0.1,
 	replaysOnErrorSampleRate: 1,
-	integrations: [new Sentry.Replay()]
+	integrations: [new Sentry.Replay()],
+	environment: env.PUBLIC_VERCEL_ENV
 });
 
 export const handleError = Sentry.handleErrorWithSentry();
--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,9 +1,11 @@
 import { sequence } from '@sveltejs/kit/hooks';
+import { env } from '$env/dynamic/public';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
 	dsn: 'https://857800ed593d481bb0da2843516d7845@o235190.ingest.sentry.io/4504617287417856',
-	tracesSampleRate: 1
+	tracesSampleRate: 1,
+	environment: env.PUBLIC_VERCEL_ENV
 });
 
 export const handleError = Sentry.handleErrorWithSentry();
