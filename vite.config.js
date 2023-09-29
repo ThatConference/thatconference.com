23ac46c23d732d9358dd8779805fc31e5db885e8
Brett Slaski <brett@thatconference.com>
2023-09-29 14:54:40 -0500

    refactor: sourcemap settings for sentry

--- vite.config.js
+++ vite.config.js
@@ -7,26 +7,28 @@ const config = ({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
 
 	const sentryConfig = {
-		url: 'https://sentry.io',
-		authToken: process.env.SENTRY_SRC_MAP_UPLOAD,
-		org: 'that-conference',
-		project: 'thatconference-com',
-		release: pkg.version,
-		deploy: {
-			env: 'production'
-		},
-		setCommits: {
-			auto: true,
-			ignoreMissing: true
-		},
-		sourceMaps: {
-			include: ['./.svelte-kit/output'],
-			ignore: ['node_modules'],
-			validate: true
-		},
-		debug: false,
-		dryRun: false,
-		cleanArtifacts: true
+		sourceMapsUploadOptions: {
+			url: 'https://sentry.io',
+			authToken: process.env.SENTRY_SRC_MAP_UPLOAD,
+			org: 'that-conference',
+			project: 'thatconference-com',
+			release: pkg.version,
+			setCommits: {
+				auto: true,
+				ignoreMissing: true
+			},
+			sourceMaps: {
+				assets: ['./.svelte-kit/output'],
+				ignore: ['node_modules'],
+				validate: true
+			},
+			deploy: {
+				env: 'production'
+			},
+			debug: false,
+			dryRun: false,
+			cleanArtifacts: true
+		}
 	};
 
 	return defineConfig({
@@ -41,3 +43,6 @@ const config = ({ mode }) => {
 };
 
 export default config;
+
+// Sentry vite plugin reference:
+// https://github.com/getsentry/sentry-javascript-bundler-plugins/tree/259018857f1fd58b29a8fa92fa573953a9c30fca/packages/vite-plugin
