8cb03aa5c1ce225f0d90385636154dc45b2c7208
Clark Sell <clark.sell@csell.net>
2023-10-19 19:24:10 -0500

    fix: broken links

--- __MIGRATION_TBD__/__events copy__/(thatConferences)/_components/nav/NavHat.svelte
+++ __MIGRATION_TBD__/__events copy__/(thatConferences)/_components/nav/NavHat.svelte
@@ -13,21 +13,21 @@
 	<div class="flex h-full items-center justify-between">
 		<div class="flex space-x-3">
 			<div class="font-semibold antialiased">
-				<a href="/" class="flex items-center space-x-2 ">
+				<a href="/" class="flex items-center space-x-2">
 					<LeftArrow />
 					<span>Back to THAT</span>
 				</a>
 			</div>
 			<div>||</div>
 			<div class="font-semibold antialiased">
-				<a href="/that-conference" class="flex items-center space-x-2 ">
+				<a href="/" class="flex items-center space-x-2">
 					<span>THAT Conference</span>
 				</a>
 			</div>
 		</div>
 		<div class="flex items-center space-x-12">
 			<div class="flex space-x-3">
-				<a href={`/that-conference`}>
+				<a href={`/`}>
 					<div class="flex space-x-4 font-semibold uppercase tracking-wide antialiased">
 						<div>THAT Conference</div>
 					</div>
