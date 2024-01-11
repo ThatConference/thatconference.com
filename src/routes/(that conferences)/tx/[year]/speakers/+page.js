e10e2dfdb481dd70185e797b0c9d45cc0f3e2748
Clark Sell <clark.sell@csell.net>
2024-01-11 06:47:43 -0600

    fix: duplicate speakers in list

--- src/routes/(that conferences)/tx/[year]/speakers/+page.js
+++ src/routes/(that conferences)/tx/[year]/speakers/+page.js
@@ -6,12 +6,18 @@ export async function load({ parent }) {
 	const speakers = sessions
 		.filter((s) => s.status === 'ACCEPTED')
 		.reduce((acc, cur) => {
-			console.log(cur);
 			acc.push(...cur.speakers);
 
 			return acc;
 		}, [])
-		.filter((s) => s.profileSlug !== 'thatconference');
+		.filter((s) => s.profileSlug !== 'thatconference')
+		.reduce((acc, cur) => {
+			if (!acc.find((speaker) => speaker.id === cur.id)) {
+				acc.push(cur);
+			}
+
+			return acc;
+		}, []);
 
 	return {
 		speakers,
--- src/routes/(that conferences)/wi/[year]/speakers/+page.js
+++ src/routes/(that conferences)/wi/[year]/speakers/+page.js
@@ -1,8 +1,26 @@
 export async function load({ parent }) {
 	let { event } = await parent();
 
+	const { sessions } = event.sessions;
+
+	const speakers = sessions
+		.filter((s) => s.status === 'ACCEPTED')
+		.reduce((acc, cur) => {
+			acc.push(...cur.speakers);
+
+			return acc;
+		}, [])
+		.filter((s) => s.profileSlug !== 'thatconference')
+		.reduce((acc, cur) => {
+			if (!acc.find((speaker) => speaker.id === cur.id)) {
+				acc.push(cur);
+			}
+
+			return acc;
+		}, []);
+
 	return {
-		speakers: event.speakers,
+		speakers,
 		event
 	};
 }
