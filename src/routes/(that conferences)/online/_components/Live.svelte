2f60ef3cd1f6969b041ab4e6a609dbd43a5500e1
Clark Sell <clark.sell@csell.net>
2024-01-29 21:21:40 -0600

    adding streamyard embed day 2

--- src/routes/(that conferences)/online/_components/Live.svelte
+++ src/routes/(that conferences)/online/_components/Live.svelte
@@ -24,7 +24,7 @@
 			<div style="width:100%;height:0px;position:relative;padding-bottom:56.25%;">
 				<iframe
 					title="THAT Conference Live Stream"
-					src="https://streamyard.com/watch/vS5mcM3smgx6?embed=true"
+					src="https://streamyard.com/watch/8TafmBy4Vxsy?embed=true"
 					width="100%"
 					height="100%"
 					frameborder="0"
