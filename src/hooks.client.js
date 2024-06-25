d6a6d88fa860599d1625a6936d09ad5fda28da35
Clark Sell <clark.sell@csell.net>
2024-06-25 06:38:37 -0500

    updating sentry api calls

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -4,14 +4,11 @@ import config, { logging } from '$lib/config.public';
 Sentry.init({
 	enabled: config.nodeEnv !== 'development',
 	dsn: logging.dsn,
-	attachStacktrace: true,
-	tracesSampleRate: 1,
-	replaysSessionSampleRate: 0.1,
-	replaysOnErrorSampleRate: 1,
-	integrations: [new Sentry.Replay()],
 	environment: logging.environment,
+	attachStacktrace: true,
 	denyUrls: logging.denyUrls,
-	enableTracing: false
+	enableTracing: false,
+	tracesSampleRate: 0
 });
 
 export const handleError = Sentry.handleErrorWithSentry();
